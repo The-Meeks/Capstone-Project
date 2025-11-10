5cac9677fde7b96a0574ce69c23a88cd4722fb7b
The-Meeks <benedictpas01@gmail.com>
2025-11-10 14:26:31 +0300

    initial setup|

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import CountBtn from '@/components/CountBtn';
+import ReactSVG from '@/assets/react.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={ReactSVG} className="App-logo" alt="logo" />
+          <p>this is react.</p>
+
+    </div>
+  );
+}
+
+export default App;
